669603951e149ad787c219d2d13fde7080eb512e
ramiro Sokolowicz <a54911@MacBook-Air-3.local>
2025-06-04 17:43:42 -0300

    genero peli/serie

---
+++ js/generoPeli.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const URLgenero = "https://api.themoviedb.org/3/genre/movie/list?api_key=2621253a21d069b5d3996a4a7b9b8692";
+    const loQueAgarroDelHtml = document.querySelector(".ulGenero");
+
+    fetch(URLgenero)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let genero = "";
+            for (let i = 0; i < data.genres.length; i++) {
+                genero += `
+                <li>
+                    <a href="detalleGenero.html?id=${data.genres[i].id}">
+                        ${data.genres[i].name}
+                    </a>
+                </li>
+                `;
+            }
+            loQueAgarroDelHtml.innerHTML = genero;
+        })
+        .catch(function (error) {
+            console.log("Error al cargar los géneros", error);
+        });
+    });
